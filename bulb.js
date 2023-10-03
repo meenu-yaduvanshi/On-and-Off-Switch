cb17d4db46e6a747d4475118cfe158de299956b0
meen- yaduvanshi <meenuyaduvanshi24@gmail.com>
2023-10-03 17:01:17 +0530

    Bulb On and Off

--- bulb.js
+++ bulb.js
@@ -1,35 +1,22 @@
 
 var buttonOff = document.querySelector("#button_off")
-let com = 0
+let com = 0;
 buttonOff.addEventListener("click", function () {
 
     if (com === 0) {
         buttonOff.textContent = "TURN ON"
         com = 1
         buttonOff.style.backgroundColor = "green"
+        var img = document.querySelector(".img_off")
+        img.setAttribute("src", "bulb-on.png")
     }
     else {
         buttonOff.textContent = "TURN OFF"
         com = 0
         buttonOff.style.backgroundColor = "white"
+        var img = document.querySelector(".img_off")
+        img.setAttribute("src", "bulb-off.png")
     }
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var imgOff = document.querySelector("#img_off")
-// var imgOn = document.querySelector("#img_on")
 
